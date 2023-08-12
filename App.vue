9dd14eb95b79e22c16ae6707a2b2e159972ddb84
georgeCx5 <69874793+georgeCx5@users.noreply.github.com>
2023-08-11 23:49:59 -0500

    Add files via upload
    - Mobile ✔
    - Tablet ⌛
    - Desktop ⏳

--- App.vue
+++ App.vue
@@ -55,50 +55,79 @@ export default {
 </script>
 <template>
   <body class=" flex flex-col items-center font-commissioner text-neo-Text">
-    <header class=" relative w-full max-w-[425px] py-[6.875rem] pb-24 overflow-hidden">
+    <header class=" relative w-full max-w-[425px] TB:max-w-[1024px] pt-[6.875rem] TB:pt-0 pb-24 TB:pb-0 overflow-hidden">
       <nav
-        class=" fixed top-0 flex justify-between items-center w-full max-w-[425px] px-6 py-10 bg-neo-white select-none z-10">
+        class=" fixed TB:relative top-0 flex justify-between items-center w-full max-w-[425px] TB:max-w-[1024px] px-6 py-10 TB:py-0 TB:pl-10 TB:pr-0 bg-neo-white select-none z-10">
         <img :src="logo" alt="logo" draggable="false">
-        <button @click=" isNavOpen = !isNavOpen">
-          <img v-show="!isNavOpen" :src="iconHamburger" alt="iconHamburger" draggable="false">
-          <img v-show="isNavOpen" :src="iconCross" alt="iconCross" draggable="false">
+        <!-- Nav Mobile -->
+        <button @click=" isNavOpen = !isNavOpen"
+          class=" relative flex TB:hidden justify-center items-center w-[24px] h-[24px]">
+          <Transition name="fade-nav">
+            <img v-show="!isNavOpen" class=" absolute" :src="iconHamburger" alt="iconHamburger" draggable="false">
+          </Transition>
+          <Transition name="fade-nav">
+            <img v-show="isNavOpen" class=" absolute" :src="iconCross" alt="iconCross" draggable="false">
+          </Transition>
         </button>
-        <div v-show="isNavOpen" class=" absolute top-full left-0 flex flex-col w-full pr-7">
-          <div class=" flex flex-col self-end items-center gap-6 px-7 pt-8 pb-7 bg-neo-BG text-neo-white">
-            <button v-for="item in navData" class=" hover:text-neo-red text-[1.125rem] leading-[2rem]">
+        <Transition name="fade-nav">
+          <div v-show="isNavOpen" class=" absolute top-full left-0 flex TB:hidden flex-col w-full pr-7">
+            <div class=" flex flex-col self-end items-center gap-6 px-7 pt-8 pb-7 bg-neo-BG text-neo-white">
+              <button v-for="item in navData" class=" hover:text-neo-red text-[1.125rem] leading-[2rem]">
+                {{ item }}
+              </button>
+              <button
+                class=" w-[199px] h-16 bg-neo-red hover:bg-white hover:text-neo-red text-[1rem] leading-[1.25rem] font-extrabold select-none">
+                Schedule a Call
+              </button>
+            </div>
+          </div>
+        </Transition>
+        <!--  -->
+        <!-- Nav Desktop -->
+        <div class=" hidden TB:flex gap-7 bg-neo-red py-14 pl-8 pr-10">
+          <div class=" flex gap-[1.25rem]">
+            <button v-for="item in navData" class=" w-14 text-neo-white text-[.9375rem] leading-[2rem] hover:font-bold">
               {{ item }}
             </button>
-            <button
-              class=" w-[199px] h-16 bg-neo-red hover:bg-white hover:text-neo-red text-[1rem] leading-[1.25rem] font-extrabold select-none">
-              Schedule a Call
-            </button>
           </div>
+          <button
+            class=" w-[147px] h-[64px] bg-neo-BG hover:bg-neo-one text-neo-white text-[1rem] leading-[1.25rem] font-extrabold">
+            Schedule a Call
+          </button>
         </div>
+        <!--  -->
       </nav>
-      <div :class="` w-full h-[200px] mb-14 ${store.joinArr(store.bgHeader)} bg-cover`"></div>
-      <div class=" px-6">
-        <h1 class=" mb-6 text-[2.5rem] leading-[2.5rem] font-extrabold">
-          Branding & website design agency</h1>
-        <p class=" mb-10 text-[.9375rem] leading-[1.5625rem]">
-          We specialize in visual storytelling by creating cohesive brand and website design
-          solutions for small businesses, giving lasting impressions to audiences in a digital world.
-        </p>
-        <button
-          class=" w-[177px] h-[72px] bg-neo-red hover:bg-opacity-60 text-neo-white text-[1.125rem] leading-[1.375rem] font-extrabold select-none">
-          Learn more
-        </button>
+      <div class=" relative flex flex-col TB:flex-row items-center TB:h-[606px]">
+        <div
+          :class="` TB:absolute TB:right-0 w-full h-[200px] TB:w-[57%] TB:h-full mb-14 TB:mb-0 ${store.joinArr(store.bgHeader)} bg-cover bg-center bg-no-repeat`">
+        </div>
+        <div class=" TB:relative TB:w-[51.75%] TB:ml-10 TB:pb-[4.25rem] px-6 TB:px-0">
+          <h1 class=" mb-6 TB:mb-7 text-[2.5rem] TB:text-[3.5rem] leading-[2.5rem] TB:leading-[3.5rem] font-extrabold">
+            Branding & website design agency</h1>
+          <p class=" mb-10 TB:mb-8 text-[.9375rem] leading-[1.5625rem]">
+            We specialize in visual storytelling by creating cohesive brand and website design
+            solutions for small businesses, giving lasting impressions to audiences in a digital world.
+          </p>
+          <button
+            class=" w-[177px] h-[72px] bg-neo-red hover:bg-opacity-60 text-neo-white text-[1.125rem] leading-[1.375rem] font-extrabold select-none">
+            Learn more
+          </button>
+        </div>
       </div>
     </header>
-    <main class=" w-full max-w-[425px] text-neo-white">
-      <section>
-        <div :class="` relative w-full h-[200px] ${store.joinArr(store.bgStrategic)} bg-cover select-none`">
-          <img class=" absolute -bottom-3 left-6 h-7" :src="pttWaveRed" alt="pttWaveRed" draggable="false">
+    <main class=" w-full max-w-[425px] TB:max-w-[1024px] text-neo-white">
+      <section class=" flex flex-col TB:flex-row items-start TB:h-[658px]">
+        <div
+          :class="` relative w-full TB:w-[49.3%] h-[200px] TB:h-full ${store.joinArr(store.bgStrategic)} bg-cover TB:bg-center select-none`">
+          <img class=" absolute -bottom-3 TB:bottom-[30rem] left-6 TB:left-auto TB:-right-6 h-7" :src="pttWaveRed"
+            alt="pttWaveRed" draggable="false">
         </div>
-        <div class=" flex flex-col justify-center items-start w-full h-[377px] px-6 bg-neo-BG">
-          <h2 class=" mb-6 text-[2rem] leading-[2.5rem] font-extrabold">
+        <div
+          class=" flex flex-col justify-center items-start w-full TB:w-[50.7%] h-[377px] TB:h-[538px] px-6 TB:px-[4.375rem] bg-neo-BG">
+          <h2 class=" mb-6 TB:mb-8 TB:pr-8 text-[2rem] leading-[2.5rem] font-extrabold">
             <span class=" text-neo-red">Design</span> is strategic.
           </h2>
-          <p class=" mb-10 text-[.9375rem] leading-[1.5625rem]">
+          <p class=" mb-10 TB:mb-8 text-[.9375rem] leading-[1.5625rem]">
             A well-crafted design strategy consistently produces desired outcomes and brand awareness.
             We are firm believers that success lies in creative collaboration with our clients.
           </p>
@@ -110,13 +139,15 @@ export default {
           </button>
         </div>
       </section>
-      <section class=" bg-neo-red px-6 py-24">
-        <h2 class=" mb-12 text-[2rem] leading-[2.5rem] font-extrabold">
+      <section class=" relative TB:flex TB:justify-end bg-neo-red TB:bg-transparent TB:mt-[-7.5rem] px-6 py-24 TB:p-0">
+        <h2
+          class=" TB:absolute TB:left-10 TB:top-64 TB:w-[36.6%] mb-12 TB:mb-0 TB:text-neo-Text text-[2rem] leading-[2.5rem] font-extrabold">
           Our approach for creating a winning brand</h2>
-        <div class=" flex flex-col gap-10">
-          <article v-for="item in listData" class=" relative px-4 py-11 text-[.9375rem] leading-[1.5625rem]">
+        <div
+          class=" flex flex-col gap-10 TB:gap-[5.25rem] TB:w-[57%] TB:bg-neo-red TB:pl-11 TB:pr-10 TB:pt-[10.5rem] TB:pb-[7rem]">
+          <article v-for="item in listData" class=" relative px-4 py-11 TB:p-0 text-[.9375rem] leading-[1.5625rem]">
             <div
-              class=" absolute left-0 top-0 text-neo-white text-opacity-25 text-[5rem] leading-[5.5rem] font-extrabold select-none">
+              class=" absolute left-0 TB:-left-4 top-0 TB:-top-11 text-neo-white text-opacity-25 text-[5rem] leading-[5.5rem] font-extrabold select-none">
               0{{ item.id }}
             </div>
             <h4 class=" mb-4 font-extrabold">
@@ -130,7 +161,7 @@ export default {
       <section>
         <div class=" relative w-full h-[404px]">
           <div v-for="(item, index) in store.bgSlides">
-            <Transition>
+            <Transition name="fade-slide">
               <div v-show="index == store.slideCount"
                 :class="` absolute w-full h-full ${store.joinArr(item.bgs)} bg-cover bg-center`">
                 <div class=" absolute bottom-0 w-full h-[111px] bg-gradient-to-b from-transparent to-neo-Text"></div>
@@ -166,13 +197,20 @@ export default {
   </body>
 </template>
 <style>
-.v-enter-active,
-.v-leave-active {
+.fade-slide-enter-active,
+.fade-slide-leave-active {
   transition: opacity .75s ease;
 }
 
-.v-enter-from,
-.v-leave-to {
+.fade-nav-enter-active,
+.fade-nav-leave-active {
+  transition: opacity .15s ease;
+}
+
+.fade-slide-enter-from,
+.fade-slide-leave-to,
+.fade-nav-enter-from,
+.fade-nav-leave-to {
   opacity: 0;
 }
 </style>
